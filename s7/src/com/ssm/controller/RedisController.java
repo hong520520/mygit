e26cf8b378f28d7b9bb64da55138abd451227749
hong520520 <205207584@qq.com>
2020-03-12 10:06:55 +0800

    洪小太爷的修改

--- s7/src/com/ssm/controller/RedisController.java
+++ s7/src/com/ssm/controller/RedisController.java
@@ -15,7 +15,6 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.ssm.entity.User;
 import com.ssm.util.CacheUtil;
-//洪小太爷的修改
 @Controller
 public class RedisController {
 	@Resource
